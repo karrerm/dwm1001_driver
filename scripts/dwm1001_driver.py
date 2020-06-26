b7819182e90342d25e30d9cb4071da7c599fc080
karrerm <karrerm@student.ethz.ch>
2020-06-26 10:36:48 +0200

    Update driver script to new onboard software and adjust sample launch file

--- scripts/dwm1001_driver.py
+++ scripts/dwm1001_driver.py
@@ -7,23 +7,40 @@ from sensor_msgs.msg import Range
 import numpy as np
 import time
 import serial
+import json
 
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 
 class MainClass(object):
     def __init__(self):
-        self.scale_factor_ = 1.0
-        self.offset_ = 0.0
+        self.scale_factor_ = {}
+        self.offset_ = {}
         self.serial_port_ = "/dev/ttyS1"
         self.baud_rate_ = 115200
-        self.topic_name_ = "uwb_distance"
+        self.this_id_ = 0
+        #self.topic_name_ = "uwb_distance"
+        self.uid_mapping_ = {}
+        self.publishers_ = {}
     def read_parameters(self):
         # Read in the parameters from ros
-        self.scale_factor_ = rospy.get_param('~scale_factor', self.scale_factor_)
-        self.offset_ = rospy.get_param('~offset', self.offset_)
+        scale_factor = rospy.get_param("~scale_factor")
+        offset = rospy.get_param("~offset")
         self.serial_port_ = rospy.get_param('~serial_port', self.serial_port_)
         self.baud_rate_ = rospy.get_param('~baud_rate', self.baud_rate_)
-        self.topic_name_ = rospy.get_param('~topic_name', self.topic_name_)
+        topic_name = "uwb_distance"
+        topic_name = rospy.get_param('~topic_name', topic_name)
+        self.this_id_ = rospy.get_param('~this_id', self.this_id_)
+        uids = rospy.get_param("~uids")
+        uids_mapping = rospy.get_param("~uids_mapping")
+        if (len(uids) != len(uids_mapping)) : print('Mappings are not correct')
+        print(scale_factor)
+        for i in range(len(uids)) :
+            self.scale_factor_[uids[i]] = scale_factor[i]
+            self.offset_[uids[i]] = offset[i]
+            self.uid_mapping_[uids[i]] = uids_mapping[i]
+            tmp_topic_name = '/%s_%d_%d' %(topic_name, self.this_id_, uids_mapping[i])
+            self.publishers_[uids[i]] = rospy.Publisher(tmp_topic_name, Range, queue_size=10)
+            print(tmp_topic_name)
 
 def main():
     rospy.init_node('dwm1001_driver_node')
@@ -31,25 +48,30 @@ def main():
     rospy.loginfo('Starting the dwm1001 driver node')
     main_class = MainClass()
     main_class.read_parameters()
-    ser = serial.Serial('/dev/serial/by-id/usb-SEGGER_J-Link_000760088177-if00', 115200, timeout=1)
-    pub = rospy.Publisher(main_class.topic_name_, Range, queue_size=10)
-    rate = rospy.Rate(1000)  # 10hz
+    ser = serial.Serial(main_class.serial_port_, 115200, timeout=1)
+    rate = rospy.Rate(200) # 1000Hz
 
+    range_msg = Range()
+    range_msg.min_range = 0.0
+    range_msg.max_range = 200.0
     while not rospy.is_shutdown():
-        range_msg = Range()
         line = ser.readline()
-        range_msg.header.stamp = rospy.get_rostime()
-        range_msg.min_range = 0.0
-        range_msg.max_range = 200.0
+        data = {}
         try:
-            val = float(line)
-            val = val / 1000.0
-            val = val * main_class.scale_factor_ + main_class.offset_
-            range_msg.range = val
-            pub.publish(range_msg)
-            rate.sleep()
-        except:
-            print('failed to read\n')
+          data = json.loads(line)
+        except ValueError as e:
+          continue
+        range_msg.header.stamp = rospy.get_rostime()
+        if 'nrng' not in data : 
+          continue
+        if 'rng' not in data['nrng'] :
+          continue
+        if 'uid' not in data['nrng'] :
+          continue
+        for i in range(len(data['nrng']['uid'])) :
+          range_msg.range = float(data['nrng']['rng'][i])
+          main_class.publishers_[int(data['nrng']['uid'][i])].publish(range_msg)
+        rate.sleep()
 
 if __name__ == '__main__':
     main()
